0f41c53ce86aa860b9a8ccbd33f1bfdccfdae18b
cnlimiter <cnlimiter@163.com>
2022-05-22 21:36:28 +0800

    try to add Infinitato

---
+++ src/main/java/nova/committee/avaritia/common/block/InfinitatoBlock.java
@@ -0,0 +1,124 @@
+package nova.committee.avaritia.common.block;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.RenderShape;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.material.Material;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
+import nova.committee.avaritia.api.common.block.BaseTileEntityBlock;
+import nova.committee.avaritia.common.tile.InfinitatoTile;
+import nova.committee.avaritia.init.registry.ModTileEntities;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.annotation.Nonnull;
+import java.util.Objects;
+
+/**
+ * Description:
+ * Author: cnlimiter
+ * Date: 2022/5/22 17:28
+ * Version: 1.0
+ */
+public class InfinitatoBlock extends BaseTileEntityBlock {
+    private static final VoxelShape SHAPE = box(8, 0, 8, 12, 8, 12);
+
+    public InfinitatoBlock() {
+        super(Material.CLOTH_DECORATION, SoundType.WOOL, 0.25F, 1F, true);
+        setRegistryName("infinitato");
+        registerDefaultState(defaultBlockState()
+                .setValue(BlockStateProperties.HORIZONTAL_FACING, Direction.SOUTH));
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        super.createBlockStateDefinition(builder);
+        builder.add(BlockStateProperties.HORIZONTAL_FACING);
+    }
+
+    @Nonnull
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext ctx) {
+        return super.getStateForPlacement(ctx).setValue(BlockStateProperties.HORIZONTAL_FACING, ctx.getHorizontalDirection().getOpposite());
+    }
+
+
+    @Override
+    public @NotNull InteractionResult use(@NotNull BlockState p_60503_, Level level, @NotNull BlockPos pos, @NotNull Player player, @NotNull InteractionHand p_60507_, @NotNull BlockHitResult p_60508_) {
+        if (!level.isClientSide()) {
+            var tile = level.getBlockEntity(pos);
+
+            if (tile instanceof InfinitatoTile t) {
+                t.interact();
+                if (!level.isClientSide) {
+                    AABB box = SHAPE.bounds();
+                    ((ServerLevel) level).sendParticles(ParticleTypes.HEART, pos.getX() + box.minX + Math.random() * (box.maxX - box.minX), pos.getY() + box.maxY, pos.getZ() + box.minZ + Math.random() * (box.maxZ - box.minZ), 1, 0, 0, 0, 0);
+                    ((ServerLevel) level).sendParticles(ParticleTypes.HEART, pos.getX() + box.minX + Math.random() * (box.maxX - box.minX), pos.getY() + box.maxY, pos.getZ() + box.minZ + Math.random() * (box.maxZ - box.minZ), 1, 0, 0, 0, 0);
+                }
+
+            }
+
+
+        }
+
+        return InteractionResult.SUCCESS;
+    }
+
+    @Override
+    public void setPlacedBy(Level pLevel, BlockPos pPos, BlockState pState, @Nullable LivingEntity pPlacer, ItemStack pStack) {
+
+        if (pStack.hasCustomHoverName()) {
+            ((InfinitatoTile) Objects.requireNonNull(pLevel.getBlockEntity(pPos))).name = pStack.getDisplayName().getString();
+        }
+    }
+
+    @Nonnull
+    @Override
+    public VoxelShape getShape(BlockState state, BlockGetter world, BlockPos pos, CollisionContext ctx) {
+        return SHAPE;
+    }
+
+    @Nullable
+    @Override
+    public BlockEntity newBlockEntity(BlockPos pPos, BlockState pState) {
+        return new InfinitatoTile(pPos, pState);
+    }
+
+    @Nonnull
+    @Override
+    public RenderShape getRenderShape(BlockState state) {
+        return RenderShape.ENTITYBLOCK_ANIMATED;
+    }
+
+
+    @Override
+    protected <T extends BlockEntity> BlockEntityTicker<T> getServerTicker(Level level, BlockState state, BlockEntityType<T> type) {
+        return createTicker(type, ModTileEntities.infinitato_tile, InfinitatoTile::tick);
+    }
+
+    @Override
+    protected <T extends BlockEntity> BlockEntityTicker<T> getClientTicker(Level level, BlockState state, BlockEntityType<T> type) {
+        return createTicker(type, ModTileEntities.infinitato_tile, InfinitatoTile::tick);
+    }
+}
