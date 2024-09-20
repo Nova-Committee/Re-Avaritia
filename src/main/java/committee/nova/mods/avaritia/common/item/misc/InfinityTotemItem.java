878068ebdf4c22b9aaab4c92a1e3f1a2696141ef
cnlimiter <cnlimiter@163.com>
2024-09-21 02:40:07 +0800

    ```fix(InfinityTotemItem): set infinite durability and prevent foil effect
    Adjust the InfinityTotemItem to have infinite durability, correcting from a high but finite
    value to truly infinite. Additionally, override the isFoil method to ensure the item does
    not have a foil effect, promoting clarity and consistency in item presentation.
    ```

--- src/main/java/committee/nova/mods/avaritia/common/item/misc/InfinityTotemItem.java
+++ src/main/java/committee/nova/mods/avaritia/common/item/misc/InfinityTotemItem.java
@@ -3,6 +3,8 @@ package committee.nova.mods.avaritia.common.item.misc;
 import committee.nova.mods.avaritia.common.item.resources.ResourceItem;
 import committee.nova.mods.avaritia.init.registry.ModRarities;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * Description:
@@ -14,7 +16,12 @@ public class InfinityTotemItem extends ResourceItem {
 
 
     public InfinityTotemItem() {
-        super(ModRarities.EPIC, "infinity_totem", true, new Item.Properties().stacksTo(1).durability(999));
+        super(ModRarities.EPIC, "infinity_totem", true, new Item.Properties().stacksTo(1).durability(99));
+    }
+
+    @Override
+    public boolean isFoil(@NotNull ItemStack pStack) {
+        return false;
     }
 
 }
