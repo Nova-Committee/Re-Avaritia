1ae7fef853c8eb4010ead7f42acad65ccdd7e72a
LDS_XiaoYe <xiaoyecreate@outlook.com>
2025-01-20 10:43:17 +0800

    Update RegisterRecipesEvent.java

--- src/main/java/committee/nova/mods/avaritia/init/event/RegisterRecipesEvent.java
+++ src/main/java/committee/nova/mods/avaritia/init/event/RegisterRecipesEvent.java
@@ -1,6 +1,5 @@
 package committee.nova.mods.avaritia.init.event;
 
-import com.google.common.collect.ImmutableMap;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.Container;
 import net.minecraft.world.item.crafting.Recipe;
@@ -11,43 +10,51 @@ import net.minecraftforge.eventbus.api.Event;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * Description:
- * Author: cnlimiter
- * Date: 2022/5/15 20:37
- * Version: 1.0
- */
 public class RegisterRecipesEvent extends Event {
     private final RecipeManager recipeManager;
-    public final Map<RecipeType<?>, Map<ResourceLocation, Recipe<?>>> oldRecipes;
-    public final Map<ResourceLocation, Recipe<?>> oldByName;
 
     public RegisterRecipesEvent(RecipeManager recipeManager) {
         this.recipeManager = recipeManager;
-        this.oldRecipes = recipeManager.recipes;
-        this.oldByName = recipeManager.byName;
     }
 
     public RecipeManager getRecipeManager() {
-        //if (recipeManager.recipes instanceof ImmutableMap) {
-            recipeManager.recipes = new HashMap<>(oldRecipes);
-            recipeManager.recipes.replaceAll((t, v) -> new HashMap<>(oldRecipes.get(t)));
-        //}
-
-        //if (recipeManager.byName instanceof ImmutableMap) {
-            recipeManager.byName = new HashMap<>(oldByName);
-        //}
-
+        if (!(recipeManager.recipes instanceof HashMap)) {
+            try {
+                Map<RecipeType<?>, Map<ResourceLocation, Recipe<?>>> newRecipes = new HashMap<>();
+                recipeManager.recipes.forEach((type, map) -> 
+                    newRecipes.put(type, new HashMap<>(map))
+                );
+                recipeManager.recipes = newRecipes;
+            } catch (Exception e) {
+                System.err.println("Failed to convert recipes: " + e.getMessage());
+            }
+        }
+        if (!(recipeManager.byName instanceof HashMap)) {
+            try {
+                recipeManager.byName = new HashMap<>(recipeManager.byName);
+            } catch (Exception e) {
+                System.err.println("Failed to convert byName: " + e.getMessage());
+            }
+        }
         return recipeManager;
     }
 
     public void addRecipe(Recipe<?> recipe) {
-        addRecipe(getRecipeManager(), recipe);
-    }
+        RecipeManager manager = getRecipeManager();
 
-    public void addRecipe(RecipeManager recipeManager, Recipe<?> recipe) {
-        recipeManager.recipes.computeIfAbsent(recipe.getType(), (t) -> new HashMap<>()).put(recipe.getId(), recipe);
-        recipeManager.byName.put(recipe.getId(), recipe);
+        try {
+            Map<RecipeType<?>, Map<ResourceLocation, Recipe<?>>> newRecipes = new HashMap<>(manager.recipes);
+            newRecipes.computeIfAbsent(recipe.getType(), t -> new HashMap<>())
+                      .put(recipe.getId(), recipe);
+
+            Map<ResourceLocation, Recipe<?>> newByName = new HashMap<>(manager.byName);
+            newByName.put(recipe.getId(), recipe);
+
+            manager.recipes = newRecipes;
+            manager.byName = newByName;
+        } catch (Exception e) {
+            System.err.println("Failed to add recipe: " + e.getMessage());
+        }
     }
 
     public <C extends Container, T extends Recipe<C>> Map<ResourceLocation, T> getRecipes(RecipeType<T> type) {
@@ -57,6 +64,5 @@ public class RegisterRecipesEvent extends Event {
     public Recipe<?> getRecipe(ResourceLocation name) {
         return getRecipeManager().byName.get(name);
     }
-
-
 }
+    
