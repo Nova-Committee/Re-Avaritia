3259ac85912d96efa03e74d4edf9beda50e2503e
LDS_XiaoYe <xiaoyecreate@outlook.com>
2025-02-21 19:31:21 +0800

    Delete comment

--- src/main/java/committee/nova/mods/avaritia/common/entity/arrow/HeavenSubArrowEntity.java
+++ src/main/java/committee/nova/mods/avaritia/common/entity/arrow/HeavenSubArrowEntity.java
@@ -75,7 +75,6 @@ public class HeavenSubArrowEntity extends Arrow {
         if (shooter != null && shooter != entity) {
             entity.hurt(ModDamageTypes.causeRandomDamage(this.shooter), HEAVEN_ARROW_DAMAGE);
         } else {
-            // 处理 shooter 为 null 的情况
             //   entity.hurt(ModDamageTypes.causeRandomDamage(entity), Float.MAX_VALUE); // 使用被击中的实体作为默认值
             System.out.println("无尽弓 - SubArrow: 射手为空或射到了自己, 无伤害。");
         }
