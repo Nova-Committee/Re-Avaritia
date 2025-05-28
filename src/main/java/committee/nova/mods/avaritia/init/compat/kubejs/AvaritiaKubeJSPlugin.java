3a176b6f4eea8aee46ed0c6765e998fddf915014
cnlimiter <cnlimiter@163.com>
2025-05-28 15:49:47 +0800

    refactor(kubejs): disable shapeless table recipe schema
    - Comment out shapeless table recipe schema registration in AvaritiaKubeJSPlugin
    - Remove ShapelessTableRecipeSchema class implementation

--- src/main/java/committee/nova/mods/avaritia/init/compat/kubejs/AvaritiaKubeJSPlugin.java
+++ src/main/java/committee/nova/mods/avaritia/init/compat/kubejs/AvaritiaKubeJSPlugin.java
@@ -1,11 +1,6 @@
 package committee.nova.mods.avaritia.init.compat.kubejs;
 
 import committee.nova.mods.avaritia.init.compat.kubejs.component.ShapedRecipePatternComponent;
-import committee.nova.mods.avaritia.init.compat.kubejs.schema.CompressRecipeSchema;
-import committee.nova.mods.avaritia.init.compat.kubejs.schema.ExtremeSmithingRecipeSchema;
-import committee.nova.mods.avaritia.init.compat.kubejs.schema.InfinityCatalystRecipeSchema;
-import committee.nova.mods.avaritia.init.compat.kubejs.schema.ShapelessTableRecipeSchema;
-import committee.nova.mods.avaritia.init.registry.ModRecipeSerializers;
 import dev.latvian.mods.kubejs.plugin.ClassFilter;
 import dev.latvian.mods.kubejs.plugin.KubeJSPlugin;
 import dev.latvian.mods.kubejs.recipe.component.RecipeComponentTypeRegistry;
@@ -33,9 +28,9 @@ public class AvaritiaKubeJSPlugin implements KubeJSPlugin {
     @Override
     public void registerRecipeSchemas(RecipeSchemaRegistry event) {
         //event.register(ModRecipeSerializers.SHAPED_CRAFT_SERIALIZER.getId(), ShapedTableRecipeSchema.SCHEMA);
-        event.register(ModRecipeSerializers.SHAPELESS_CRAFT_SERIALIZER.getId(), ShapelessTableRecipeSchema.SCHEMA);
+        //event.register(ModRecipeSerializers.SHAPELESS_CRAFT_SERIALIZER.getId(), ShapelessTableRecipeSchema.SCHEMA);
         //event.register(ModRecipeSerializers.COMPRESSOR_SERIALIZER.getId(), CompressRecipeSchema.SCHEMA);
-        event.register(ModRecipeSerializers.INFINITY_CATALYST_CRAFT_SERIALIZER.getId(), InfinityCatalystRecipeSchema.SCHEMA);
-        event.register(ModRecipeSerializers.EXTREME_SMITHING_SERIALIZER.getId(), ExtremeSmithingRecipeSchema.SCHEMA);
+       // event.register(ModRecipeSerializers.INFINITY_CATALYST_CRAFT_SERIALIZER.getId(), InfinityCatalystRecipeSchema.SCHEMA);
+        //event.register(ModRecipeSerializers.EXTREME_SMITHING_SERIALIZER.getId(), ExtremeSmithingRecipeSchema.SCHEMA);
     }
 }
