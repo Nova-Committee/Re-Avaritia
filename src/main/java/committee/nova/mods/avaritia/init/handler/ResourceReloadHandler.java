de03756795643df273c571bf62de0b0673d7beb6
cnlimiter <cnlimiter@163.com>
2025-06-11 20:53:20 +0800

    feat(resourcepack): 添加资源包动画配置
    - 为多个资源包中的纹理添加动画配置文件
    - 包括方块、物品、工具等不同类型的动画
    -优化了动画帧时间和序列，提升了视觉效果

--- src/main/java/committee/nova/mods/avaritia/init/handler/ResourceReloadHandler.java
+++ src/main/java/committee/nova/mods/avaritia/init/handler/ResourceReloadHandler.java
@@ -1,6 +1,20 @@
 package committee.nova.mods.avaritia.init.handler;
 
-import net.neoforged.neoforge.event.AddReloadListenerEvent;
+import committee.nova.mods.avaritia.Const;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.packs.PackLocationInfo;
+import net.minecraft.server.packs.PackSelectionConfig;
+import net.minecraft.server.packs.PackType;
+import net.minecraft.server.packs.PathPackResources;
+import net.minecraft.server.packs.repository.KnownPack;
+import net.minecraft.server.packs.repository.Pack;
+import net.minecraft.server.packs.repository.PackSource;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModList;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.AddPackFindersEvent;
+
+import java.util.Optional;
 
 /**
  * Description:
@@ -8,10 +22,24 @@ import net.neoforged.neoforge.event.AddReloadListenerEvent;
  * Date: 2022/5/15 11:40
  * Version: 1.0
  */
+@EventBusSubscriber(modid = Const.MOD_ID, bus = EventBusSubscriber.Bus.MOD)
 public class ResourceReloadHandler {
 
+    @SubscribeEvent
+    public static void addPackFinders(AddPackFindersEvent event) {
+        if (event.getPackType() == PackType.CLIENT_RESOURCES) {
+            var resourcePath = ModList.get().getModFileById(Const.MOD_ID).getFile().findResource("resourcepacks/avaritia_vanilla");
+            var supplier = new PathPackResources.PathResourcesSupplier(resourcePath);
 
-    public static void onAddReloadListeners(AddReloadListenerEvent event) {
-
+            event.addRepositorySource(packConsumer -> {
+                final PackLocationInfo packInfo = new PackLocationInfo(
+                        "builtin/avaritia_vanilla",
+                        Component.literal("Re:Avaritia Vanilla"),
+                        PackSource.BUILT_IN,
+                        Optional.of(new KnownPack(Const.MOD_ID, "builtin/avaritia_vanilla", "1.0")));
+                final PackSelectionConfig selectionConfig = new PackSelectionConfig(false, Pack.Position.TOP, false);
+                packConsumer.accept(Pack.readMetaAndCreate(packInfo, supplier, PackType.CLIENT_RESOURCES, selectionConfig));
+            });
+        }
     }
 }
