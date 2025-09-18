1a706bbb35c9317698b5ce75c380c9414d516c1f
cnlimiter <cnlimiter@163.com>
2025-09-18 09:14:54 +0800

    feat(particles): add charge particle and register particle type
    - Add ChargeParticle class to handle the new particle effect- Register particle type in ModParticles class
    - Update client setup to include new particle provider
    - Adjust build.gradle for proper project configuration

---
+++ src/main/java/committee/nova/mods/avaritia/client/particle/ChargeParticle.java
@@ -0,0 +1,43 @@
+package committee.nova.mods.avaritia.client.particle;
+
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.particle.*;
+import net.minecraft.core.particles.SimpleParticleType;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * @author: cnlimiter
+ */
+public class ChargeParticle extends TextureSheetParticle {
+    private final SpriteSet sprites;
+
+    public ChargeParticle(ClientLevel world, double x, double y, double z, SpriteSet sprites) {
+        super(world, x, y, z);
+        this.lifetime = 8 + this.random.nextInt(4);
+        this.setParticleSpeed(0D, 0D, 0D);
+        this.scale(1.25F);
+        this.sprites = sprites;
+        this.setSpriteFromAge(sprites);
+    }
+
+    @Override
+    public void tick() {
+        if(this.age++ >= this.lifetime) {
+            this.remove();
+        }
+        else {
+            this.setSpriteFromAge(sprites);
+        }
+    }
+
+    @Override
+    public @NotNull ParticleRenderType getRenderType() {
+        return ParticleRenderType.PARTICLE_SHEET_LIT;
+    }
+
+    public record Factory(SpriteSet sprites) implements ParticleProvider<SimpleParticleType> {
+        public Particle createParticle(@NotNull SimpleParticleType simpleParticleType, @NotNull ClientLevel world, double x, double y, double z, double velX, double velY, double velZ) {
+            return new ChargeParticle(world, x, y, z, sprites);
+        }
+    }
+}
