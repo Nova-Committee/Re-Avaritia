8cd16b4420da1098b9a5549ee469c72f8e62f609
cu6 <3077476144@qq.com>
2025-09-18 13:54:26 +0800

    Fix JEI

--- src/main/java/committee/nova/mods/avaritia/init/compat/jei/category/tables/NetherCraftingTableCategory.java
+++ src/main/java/committee/nova/mods/avaritia/init/compat/jei/category/tables/NetherCraftingTableCategory.java
@@ -104,7 +104,7 @@ public class NetherCraftingTableCategory implements IRecipeCategory<ITierCraftin
             shapelessRecipe(builder, inputs);
             builder.addSlot(RecipeIngredientRole.OUTPUT, 133, 40).addItemStack(new ItemStack(ModItems.eternal_singularity.get()));
         }
-        builder.moveRecipeTransferButton(145, 105);
+        builder.moveRecipeTransferButton(136, 86);
     }
 
     private void shapelessRecipe(@NotNull IRecipeLayoutBuilder builder, NonNullList<Ingredient> inputs) {
