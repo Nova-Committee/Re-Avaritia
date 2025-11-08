5ef416d75d74be7bb9fd037a956a71cd732b3446
SrNadien 🔥 <76491773+SrNadien@users.noreply.github.com>
2025-11-08 14:09:42 -0300

    Updated Kubejs 2101.7.2-build.309
    Added compatibility with KubeJS 2101.7.2-build.309
    
    For now, InfinityCatalystRecipeSchema has been removed and will be re-added in a future update.
    
    Fix https://github.com/Nova-Committee/Re-Avaritia/issues/253
    
    Fix https://github.com/Nova-Committee/Re-Avaritia/issues/257

--- src/main/java/committee/nova/mods/avaritia/init/compat/kubejs/ModKubeRecipe.java
+++ src/main/java/committee/nova/mods/avaritia/init/compat/kubejs/ModKubeRecipe.java
@@ -10,7 +10,8 @@ import java.util.function.Supplier;
  * @Project: Avaritia
  * @Author: cnlimiter
  * @CreateTime: 2025/5/18 22:16
- * @Description:
+ * @Description: Clase base para recetas personalizadas de Avaritia integradas con KubeJS.
+ * KUBEJS 2101.7.2-build.309 COMPATIBILITY BY SrNadien.
  */
 public abstract class ModKubeRecipe extends KubeRecipe {
     @HideFromJS
@@ -21,12 +22,16 @@ public abstract class ModKubeRecipe extends KubeRecipe {
         return getValue(key);
     }
 
+    /**
+     * Cada receta concreta deberá definir su validación.
+     */
     protected abstract void validate();
 
-    @Override
-    public void afterLoaded() {
-        super.afterLoaded();
-
+    /**
+     * Ya no se usa @Override porque afterLoaded() fue eliminado de KubeRecipe.
+     * La validación deberá ejecutarse manualmente o desde el evento AfterRecipesLoadedKubeEvent.
+     */
+    public void onAfterRecipesLoaded() {
         this.validate();
     }
 }
