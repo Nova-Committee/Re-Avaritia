8fca17449a6262f9328a17871b8886c5e71fd9b4
cnlimiter <cnlimiter@163.com>
2025-11-26 12:25:53 +0800

    chore(version): 更新模组版本标签至beta3
    - 将mod_version_tag从beta2更新为beta3- 更新了ModSingularities中的标签引用前缀从forge改为c

--- src/main/java/committee/nova/mods/avaritia/init/registry/ModSingularities.java
+++ src/main/java/committee/nova/mods/avaritia/init/registry/ModSingularities.java
@@ -30,19 +30,19 @@ public class ModSingularities {
     public static final Singularity AMETHYST_SHARD = Singularity.create(ResourceLocation.fromNamespaceAndPath(Const.MOD_ID, "amethyst_shard"), "singularity.avaritia.amethyst_shard", new int[]{0xCFA0F3, 0x8D6ACC}, Ingredient.of(Items.AMETHYST_SHARD));
     public static final Singularity NETHERITE = Singularity.create(ResourceLocation.fromNamespaceAndPath(Const.MOD_ID, "netherite"), "singularity.avaritia.netherite", new int[]{0x443a3b, 0x1a1616}, Ingredient.of(Items.NETHERITE_INGOT));
 
-    public static final Singularity ALUMINUM = Singularity.create(ResourceLocation.fromNamespaceAndPath(Const.MOD_ID, "aluminum"), "singularity.avaritia.aluminum", new int[]{0xCACCDA, 0x9A9CA6}, "forge:ingots/aluminum");
-    public static final Singularity TIN = Singularity.create(ResourceLocation.fromNamespaceAndPath(Const.MOD_ID, "tin"), "singularity.avaritia.tin", new int[]{0xA0BEBD, 0x527889}, "forge:ingots/tin");
-    public static final Singularity BRONZE = Singularity.create(ResourceLocation.fromNamespaceAndPath(Const.MOD_ID, "bronze"), "singularity.avaritia.bronze", new int[]{0xD99F43, 0xBB6B3B}, "forge:ingots/bronze");
-    public static final Singularity SILVER = Singularity.create(ResourceLocation.fromNamespaceAndPath(Const.MOD_ID, "silver"), "singularity.avaritia.silver", new int[]{0xC0CDD2, 0x5F6E7C}, "forge:ingots/silver");
-    public static final Singularity LEAD = Singularity.create(ResourceLocation.fromNamespaceAndPath(Const.MOD_ID, "lead"), "singularity.avaritia.lead", new int[]{0x6C7D92, 0x323562}, "forge:ingots/lead");
-    public static final Singularity STEEL = Singularity.create(ResourceLocation.fromNamespaceAndPath(Const.MOD_ID, "steel"), "singularity.avaritia.steel", new int[]{0x565656, 0x232323}, "forge:ingots/steel");
-    public static final Singularity NICKEL = Singularity.create(ResourceLocation.fromNamespaceAndPath(Const.MOD_ID, "nickel"), "singularity.avaritia.nickel", new int[]{0xE1D798, 0xB1976C}, "forge:ingots/nickel");
-    public static final Singularity ELECTRUM = Singularity.create(ResourceLocation.fromNamespaceAndPath(Const.MOD_ID, "electrum"), "singularity.avaritia.electrum", new int[]{0xF5F18E, 0x9E8D3E}, "forge:ingots/electrum");
-    public static final Singularity INVAR = Singularity.create(ResourceLocation.fromNamespaceAndPath(Const.MOD_ID, "invar"), "singularity.avaritia.invar", new int[]{0xBCC5BB, 0x5D7877}, "forge:ingots/invar");
-    public static final Singularity PLATINUM = Singularity.create(ResourceLocation.fromNamespaceAndPath(Const.MOD_ID, "platinum"), "singularity.avaritia.platinum", new int[]{0x6FEAEF, 0x57B8BC}, "forge:ingots/platinum");
-    public static final Singularity URANIUM = Singularity.create(ResourceLocation.fromNamespaceAndPath(Const.MOD_ID, "uranium"), "singularity.avaritia.uranium", new int[]{0xd2f9d1, 0xa6c5a4}, "forge:ingots/uranium");
-    public static final Singularity OSMIUM = Singularity.create(ResourceLocation.fromNamespaceAndPath(Const.MOD_ID, "osmium"), "singularity.avaritia.osmium", new int[]{0xe6eef7, 0xc0c4cd}, "forge:ingots/osmium");
-    public static final Singularity REFINED_OBSIDIAN = Singularity.create(ResourceLocation.fromNamespaceAndPath(Const.MOD_ID, "refined_obsidian"), "singularity.avaritia.refined_obsidian", new int[]{0xa9a1b8, 0x8d78b7}, "forge:ingots/refined_obsidian");
+    public static final Singularity ALUMINUM = Singularity.create(ResourceLocation.fromNamespaceAndPath(Const.MOD_ID, "aluminum"), "singularity.avaritia.aluminum", new int[]{0xCACCDA, 0x9A9CA6}, "c:ingots/aluminum");
+    public static final Singularity TIN = Singularity.create(ResourceLocation.fromNamespaceAndPath(Const.MOD_ID, "tin"), "singularity.avaritia.tin", new int[]{0xA0BEBD, 0x527889}, "c:ingots/tin");
+    public static final Singularity BRONZE = Singularity.create(ResourceLocation.fromNamespaceAndPath(Const.MOD_ID, "bronze"), "singularity.avaritia.bronze", new int[]{0xD99F43, 0xBB6B3B}, "c:ingots/bronze");
+    public static final Singularity SILVER = Singularity.create(ResourceLocation.fromNamespaceAndPath(Const.MOD_ID, "silver"), "singularity.avaritia.silver", new int[]{0xC0CDD2, 0x5F6E7C}, "c:ingots/silver");
+    public static final Singularity LEAD = Singularity.create(ResourceLocation.fromNamespaceAndPath(Const.MOD_ID, "lead"), "singularity.avaritia.lead", new int[]{0x6C7D92, 0x323562}, "c:ingots/lead");
+    public static final Singularity STEEL = Singularity.create(ResourceLocation.fromNamespaceAndPath(Const.MOD_ID, "steel"), "singularity.avaritia.steel", new int[]{0x565656, 0x232323}, "c:ingots/steel");
+    public static final Singularity NICKEL = Singularity.create(ResourceLocation.fromNamespaceAndPath(Const.MOD_ID, "nickel"), "singularity.avaritia.nickel", new int[]{0xE1D798, 0xB1976C}, "c:ingots/nickel");
+    public static final Singularity ELECTRUM = Singularity.create(ResourceLocation.fromNamespaceAndPath(Const.MOD_ID, "electrum"), "singularity.avaritia.electrum", new int[]{0xF5F18E, 0x9E8D3E}, "c:ingots/electrum");
+    public static final Singularity INVAR = Singularity.create(ResourceLocation.fromNamespaceAndPath(Const.MOD_ID, "invar"), "singularity.avaritia.invar", new int[]{0xBCC5BB, 0x5D7877}, "c:ingots/invar");
+    public static final Singularity PLATINUM = Singularity.create(ResourceLocation.fromNamespaceAndPath(Const.MOD_ID, "platinum"), "singularity.avaritia.platinum", new int[]{0x6FEAEF, 0x57B8BC}, "c:ingots/platinum");
+    public static final Singularity URANIUM = Singularity.create(ResourceLocation.fromNamespaceAndPath(Const.MOD_ID, "uranium"), "singularity.avaritia.uranium", new int[]{0xd2f9d1, 0xa6c5a4}, "c:ingots/uranium");
+    public static final Singularity OSMIUM = Singularity.create(ResourceLocation.fromNamespaceAndPath(Const.MOD_ID, "osmium"), "singularity.avaritia.osmium", new int[]{0xe6eef7, 0xc0c4cd}, "c:ingots/osmium");
+    public static final Singularity REFINED_OBSIDIAN = Singularity.create(ResourceLocation.fromNamespaceAndPath(Const.MOD_ID, "refined_obsidian"), "singularity.avaritia.refined_obsidian", new int[]{0xa9a1b8, 0x8d78b7}, "c:ingots/refined_obsidian");
 
     public static List<Singularity> getDefaults() {
         return List.of(
