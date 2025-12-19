d41710ae2c3e35410f0c004957d3f46c2db868f8
cnlimiter <cnlimiter@163.com>
2025-12-20 01:42:15 +0800

    feat(mixin): 注入服务器资源加载逻辑以支持SingularityReloadListener
    - 在ReloadableServerResourcesMixin中添加对ResourceManager的引用
    - 修改注入点以捕获局部变量并调整方法签名
    - 将SingularityReloadListener插入到资源加载监听器列表中
    - 确保SingularityReloadListener在RecipeManager之前执行
    - 更新导入语句以包含必要的Java并发包类

--- src/main/java/committee/nova/mods/avaritia/init/mixins/ReloadableServerResourcesMixin.java
+++ src/main/java/committee/nova/mods/avaritia/init/mixins/ReloadableServerResourcesMixin.java
@@ -7,6 +7,7 @@ import net.minecraft.server.ReloadableServerResources;
 import net.minecraft.server.ServerAdvancementManager;
 import net.minecraft.server.ServerFunctionLibrary;
 import net.minecraft.server.packs.resources.PreparableReloadListener;
+import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.tags.TagManager;
 import net.minecraft.world.flag.FeatureFlagSet;
 import net.minecraft.world.item.crafting.RecipeManager;
@@ -19,8 +20,11 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
 
 /**
  * @Project: Avaritia
@@ -63,10 +67,12 @@ public abstract class ReloadableServerResourcesMixin {
     }
 
     @Inject(
-            at = {@At(value = "RETURN")},
-            method = {"listeners"},
-            cancellable = true)
-    public void avaritia$listeners(CallbackInfoReturnable<List<PreparableReloadListener>> cir) {
-        cir.setReturnValue(List.of(this.tagManager, this.lootData, SingularityReloadListener.INSTANCE, this.recipes, this.functionLibrary, this.advancements));
+            at = {@At(value = "INVOKE_ASSIGN", target = "Ljava/util/List;addAll(Ljava/util/Collection;)Z")},
+            method = {"loadResources"},
+            locals = LocalCapture.CAPTURE_FAILHARD
+    )
+    private static void avaritia$loadResources(ResourceManager resourceManager, RegistryAccess.Frozen registryAccess, FeatureFlagSet enabledFeatures, Commands.CommandSelection commandSelection, int functionCompilationLevel, Executor backgroundExecutor, Executor gameExecutor, CallbackInfoReturnable<CompletableFuture<ReloadableServerResources>> cir, ReloadableServerResources reloadableserverresources,
+                                           List<PreparableReloadListener> listeners) {
+        listeners.add(2, SingularityReloadListener.INSTANCE);
     }//确保SingularityReloadListener在RecipeManager之前执行
 }
