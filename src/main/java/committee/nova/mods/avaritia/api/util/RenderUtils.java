a144f656cda5ee576cc2273d1f3b3050ea5233c2
cu6 <3077476144@qq.com>
2026-01-15 23:34:05 +0800

    修复星空渲染边角料

---
+++ src/main/java/committee/nova/mods/avaritia/api/util/RenderUtils.java
@@ -0,0 +1,28 @@
+package committee.nova.mods.avaritia.api.util;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.RenderStateShard.TextureStateShard;
+import net.minecraft.client.renderer.texture.TextureAtlas;
+import net.minecraft.world.inventory.InventoryMenu;
+import org.lwjgl.opengl.GL13;
+
+public class RenderUtils {
+    public static final TextureStateShard COSMIC_TEXTURE_ISOLATED = new TextureStateShard(InventoryMenu.BLOCK_ATLAS,false,false){
+        @Override
+        public void setupRenderState() {
+            super.setupRenderState();
+            RenderSystem.activeTexture(GL13.GL_TEXTURE0);
+
+            Minecraft mc = Minecraft.getInstance();
+            TextureAtlas textureAtlas = mc.getModelManager().getAtlas(InventoryMenu.BLOCK_ATLAS);
+
+            RenderSystem.bindTexture(textureAtlas.getId());
+        }
+
+        @Override
+        public void clearRenderState() {
+            super.clearRenderState();
+        }
+    };
+}
--- src/main/java/committee/nova/mods/avaritia/client/shader/AvaritiaRenderTypes.java
+++ src/main/java/committee/nova/mods/avaritia/client/shader/AvaritiaRenderTypes.java
@@ -4,6 +4,7 @@ import com.mojang.blaze3d.vertex.DefaultVertexFormat;
 import com.mojang.blaze3d.vertex.VertexFormat;
 import committee.nova.mods.avaritia.Const;
 import committee.nova.mods.avaritia.Res;
+import committee.nova.mods.avaritia.api.util.RenderUtils;
 import net.minecraft.client.renderer.RenderStateShard;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.resources.ResourceLocation;
@@ -44,7 +45,7 @@ public class AvaritiaRenderTypes {
                     .setDepthTestState(RenderStateShard.EQUAL_DEPTH_TEST)
                     .setLightmapState(RenderStateShard.LIGHTMAP)
                     .setTransparencyState(RenderStateShard.TRANSLUCENT_TRANSPARENCY)
-                    .setTextureState(RenderStateShard.BLOCK_SHEET_MIPPED)
+                    .setTextureState(RenderUtils.COSMIC_TEXTURE_ISOLATED)
                     .createCompositeState(true)
     );
 
@@ -102,7 +103,7 @@ public class AvaritiaRenderTypes {
                     .setDepthTestState(RenderStateShard.EQUAL_DEPTH_TEST)
                     .setLightmapState(RenderStateShard.LIGHTMAP)
                     .setTransparencyState(RenderStateShard.TRANSLUCENT_TRANSPARENCY)
-                    .setTextureState(RenderStateShard.BLOCK_SHEET_MIPPED)
+                    .setTextureState(RenderUtils.COSMIC_TEXTURE_ISOLATED)
                     .createCompositeState(true)
     );
 
@@ -114,7 +115,7 @@ public class AvaritiaRenderTypes {
                     .setDepthTestState(RenderStateShard.EQUAL_DEPTH_TEST)
                     .setLightmapState(RenderStateShard.LIGHTMAP)
                     .setTransparencyState(RenderStateShard.TRANSLUCENT_TRANSPARENCY)
-                    .setTextureState(RenderStateShard.BLOCK_SHEET_MIPPED)
+                    .setTextureState(RenderUtils.COSMIC_TEXTURE_ISOLATED)
                     .createCompositeState(true)
     );
 
@@ -126,7 +127,7 @@ public class AvaritiaRenderTypes {
                     .setDepthTestState(RenderStateShard.EQUAL_DEPTH_TEST)
                     .setLightmapState(RenderStateShard.LIGHTMAP)
                     .setTransparencyState(RenderStateShard.TRANSLUCENT_TRANSPARENCY)
-                    .setTextureState(RenderStateShard.BLOCK_SHEET_MIPPED)
+                    .setTextureState(RenderUtils.COSMIC_TEXTURE_ISOLATED)
                     .createCompositeState(true)
     );
 
