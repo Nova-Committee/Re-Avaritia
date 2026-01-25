3eb5737132ce7e001a31e3eeca76b48432395252
cu6 <3077476144@qq.com>
2026-01-26 00:09:59 +0800

    Revert "refactor(tile): 重构库存管理实现以使用Forge ItemStackHandler"
    This reverts commit eec2605b57c48367db51157d51265701153399e1.

--- src/main/java/committee/nova/mods/avaritia/api/common/inventory/OnContentsChangedFunction.java
+++
@@ -1,9 +0,0 @@
-package committee.nova.mods.avaritia.api.common.inventory;
-
-/**
- * @author cnlimiter
- */
-@FunctionalInterface
-public interface OnContentsChangedFunction {
-    void apply(int i);
-}
