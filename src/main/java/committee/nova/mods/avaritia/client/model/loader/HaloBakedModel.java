1698efa60080353d8bdfa8e232ed4ee558079440
cu6 <3077476144@qq.com>
2026-02-11 19:44:05 +0800

    修复奇点显示原材料异常

--- src/main/java/committee/nova/mods/avaritia/client/model/loader/HaloBakedModel.java
+++ src/main/java/committee/nova/mods/avaritia/client/model/loader/HaloBakedModel.java
@@ -44,14 +44,14 @@ public class HaloBakedModel extends WrappedItemModel {
     @Override
     public void renderItem(ItemStack stack, ItemDisplayContext itemDisplayContext, PoseStack pPoseStack, MultiBufferSource bufferSource,
                            int packedLight, int packedOverlay) {
-        if (stack.getItem() instanceof SingularityItem
+        if (itemDisplayContext == ItemDisplayContext.GUI && stack.getItem() instanceof SingularityItem
                 && Screen.hasShiftDown()
                 && SingularityUtils.getSingularity(stack) != null) {
             var itemRender= Minecraft.getInstance().getItemRenderer();
             BakedModel bakedModel = itemRender.getModel(SingularityUtils.getSingularity(stack).getIngredient().getItems()[0],null,null,1);
             pPoseStack.pushPose();
             pPoseStack.translate(0.5, 0.5, 0);
-            itemRender.render(SingularityUtils.getSingularity(stack).getIngredient().getItems()[0], ItemDisplayContext.NONE, false, pPoseStack, bufferSource, packedLight, packedOverlay, bakedModel);
+            itemRender.render(SingularityUtils.getSingularity(stack).getIngredient().getItems()[0], ItemDisplayContext.GUI, false, pPoseStack, bufferSource, packedLight, packedOverlay, bakedModel);
             pPoseStack.popPose();
         } else {
             if (stack.getItem() instanceof IToolTransform) {
