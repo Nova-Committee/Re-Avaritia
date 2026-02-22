19f008b47652c1d29306d7723284b5d01422d0ec
Frostbite-time <81908334+Frostbite-time@users.noreply.github.com>
2026-02-22 18:01:49 +0800

    同步1.20.1有关于ICompressorRecipe的更改

--- src/main/java/committee/nova/mods/avaritia/api/common/crafting/ICompressorRecipe.java
+++ src/main/java/committee/nova/mods/avaritia/api/common/crafting/ICompressorRecipe.java
@@ -1,9 +1,12 @@
 package committee.nova.mods.avaritia.api.common.crafting;
 
 import net.minecraft.world.item.crafting.CraftingInput;
+import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.item.crafting.Recipe;
 
 public interface ICompressorRecipe extends Recipe<CraftingInput> {
+    Ingredient getInput();
+
     int getInputCount();
 
     int getTimeCost();
