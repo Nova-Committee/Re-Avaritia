446bed595bcf527721e360babbb557e565fca97a
HowXu <dev@howxu.cn>
2026-07-31 13:30:07 +0800

    Fix Infinity Sword GameTest registry serialization

--- src/main/java/committee/nova/mods/avaritia/gametest/InfinitySwordGameTests.java
+++ src/main/java/committee/nova/mods/avaritia/gametest/InfinitySwordGameTests.java
@@ -1,6 +1,5 @@
 package committee.nova.mods.avaritia.gametest;
 
-import com.mojang.serialization.MapCodec;
 import committee.nova.mods.avaritia.Const;
 import committee.nova.mods.avaritia.common.item.tools.infinity.InfinitySwordItem;
 import committee.nova.mods.avaritia.init.config.ModConfig;
@@ -8,14 +7,13 @@ import committee.nova.mods.avaritia.init.registry.ModItems;
 import committee.nova.mods.avaritia.util.ToolUtils;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Holder;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.gametest.framework.FunctionGameTestInstance;
 import net.minecraft.gametest.framework.GameTestHelper;
-import net.minecraft.gametest.framework.GameTestInstance;
 import net.minecraft.gametest.framework.TestData;
 import net.minecraft.gametest.framework.TestEnvironmentDefinition;
-import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.GameType;
@@ -23,6 +21,7 @@ import net.minecraft.world.phys.Vec3;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.RegisterGameTestsEvent;
+import net.neoforged.neoforge.registries.RegisterEvent;
 
 import java.util.function.Consumer;
 
@@ -30,23 +29,35 @@ import java.util.function.Consumer;
 @EventBusSubscriber(modid = Const.MOD_ID)
 public final class InfinitySwordGameTests {
     private static final Identifier EMPTY_STRUCTURE = Identifier.withDefaultNamespace("empty");
+    private static final Identifier ENDER_DRAGON_PART_ID = Const.rl("infinity_sword_ender_dragon_part");
+    private static final Identifier RANGED_KILL_ID = Const.rl("infinity_sword_ranged_kill");
+    private static final ResourceKey<Consumer<GameTestHelper>> ENDER_DRAGON_PART = ResourceKey.create(
+            Registries.TEST_FUNCTION, ENDER_DRAGON_PART_ID);
+    private static final ResourceKey<Consumer<GameTestHelper>> RANGED_KILL = ResourceKey.create(
+            Registries.TEST_FUNCTION, RANGED_KILL_ID);
 
     private InfinitySwordGameTests() {
+    }
+
+    @SubscribeEvent
+    public static void registerFunctions(RegisterEvent event) {
+        event.register(Registries.TEST_FUNCTION, ENDER_DRAGON_PART_ID,
+                () -> InfinitySwordGameTests::killsEnderDragonThroughPart);
+        event.register(Registries.TEST_FUNCTION, RANGED_KILL_ID,
+                () -> InfinitySwordGameTests::killsRangedTarget);
     }
 
     @SubscribeEvent
     public static void register(RegisterGameTestsEvent event) {
         Holder<TestEnvironmentDefinition<?>> environment = event.registerEnvironment(
                 Const.rl("infinity_sword"), new TestEnvironmentDefinition.AllOf());
-        event.registerTest(Const.rl("infinity_sword_ender_dragon_part"),
-                runtimeTest(environment, InfinitySwordGameTests::killsEnderDragonThroughPart));
-        event.registerTest(Const.rl("infinity_sword_ranged_kill"),
-                runtimeTest(environment, InfinitySwordGameTests::killsRangedTarget));
+        event.registerTest(ENDER_DRAGON_PART_ID, runtimeTest(ENDER_DRAGON_PART, environment));
+        event.registerTest(RANGED_KILL_ID, runtimeTest(RANGED_KILL, environment));
     }
 
-    private static GameTestInstance runtimeTest(
-            Holder<TestEnvironmentDefinition<?>> environment, Consumer<GameTestHelper> test) {
-        return new RuntimeGameTest(new TestData<>(environment, EMPTY_STRUCTURE, 40, 0, true), test);
+    private static FunctionGameTestInstance runtimeTest(
+            ResourceKey<Consumer<GameTestHelper>> test, Holder<TestEnvironmentDefinition<?>> environment) {
+        return new FunctionGameTestInstance(test, new TestData<>(environment, EMPTY_STRUCTURE, 40, 0, true));
     }
 
     private static void killsEnderDragonThroughPart(GameTestHelper helper) {
@@ -80,28 +91,4 @@ public final class InfinitySwordGameTests {
         helper.succeed();
     }
 
-    private static final class RuntimeGameTest extends GameTestInstance {
-        private final Consumer<GameTestHelper> test;
-
-        private RuntimeGameTest(
-                TestData<Holder<TestEnvironmentDefinition<?>>> data, Consumer<GameTestHelper> test) {
-            super(data);
-            this.test = test;
-        }
-
-        @Override
-        public void run(GameTestHelper helper) {
-            test.accept(helper);
-        }
-
-        @Override
-        public MapCodec<? extends GameTestInstance> codec() {
-            return FunctionGameTestInstance.CODEC;
-        }
-
-        @Override
-        protected MutableComponent typeDescription() {
-            return Component.literal("Avaritia runtime regression");
-        }
-    }
 }
